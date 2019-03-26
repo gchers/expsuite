2bb5e2ca9a27cab2804d78841fddfe45f075b25a
gchers <giovanni.cherubin@epfl.ch>
2019-03-26 18:41:40 +0000

    Remove automatic installation of requirements
    Was buggy

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 from setuptools import setup
-from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt', session='hack')
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='Python Experiment Suite',
       version='0.1',
       author='Thomas Rückstieß',
       description='A Python experiment suite',
       packages=['expsuite'],
-      install_requires=reqs,
 )
